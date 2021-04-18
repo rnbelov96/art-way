bc8cda7b5fc3cffef9d31ec8562b7947acef0f5e
Belov Roman <rnbelov96@gmail.com>
2021-04-18 20:09:17 +0300

    Инициирует проект

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import './scss/style.scss';
+import './ts/slider';
+import './ts/slider-review';
+import './ts/calc';
+import './ts/modals';
+import './ts/setup-forms';
+import './ts/f5leads-init';
+import './ts/f5leads';
