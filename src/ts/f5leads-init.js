35058f64469d5cd79fd9d6212698626eb39374bf
Roman Belov <rnbelov96@gmail.com>
2021-05-11 12:49:17 +0300

    Изменяет почту для отправки лидов

--- src/ts/f5leads-init.js
+++ src/ts/f5leads-init.js
@@ -1,5 +1,5 @@
 document.f5leads = {};
-document.f5leads.emails = '438993@mail.ru, franchise.tmn@gmail.com, leads@conversionpro.agency'; // comma separated
+document.f5leads.emails = 'artwayfranchise@yandex.ru, leads@conversionpro.agency'; // comma separated
 document.f5leads.send_to_bitrix = 'false';
 document.f5leads.expect_second_form = '0';
 document.f5leads.counterId = '';
